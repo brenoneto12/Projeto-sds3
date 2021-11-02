3262a2771d7276bbc3308af09078a726e5b3d082
Breno Neto <brenoneto14@hotmail.com>
2021-11-02 16:09:57 -0300

    Statics charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import DonutChart from "components/DonutChart";
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
@@ -6,12 +8,32 @@ function App() {
   return (
     <>
       <NavBar />
+
       <div className="container">
-        <h1 className="text-primary">"Olá Mundo !" </h1>
+        <h1 className="text-center text-primary py-3">Dashboard de Vendas Spring React Show !!! </h1>
+
+
+        <div className="row px-3">
+          <div className="col -sm -6">
+            <h5 className="text-center text_secundary">Positivação das Vendas (%) !</h5>
+            <BarChart />
+          </div>  
+          <div className="col -sm -6">
+            <h5 className="text-center text_secundary">Participação por Vendedor (%)!</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+
+        <div className="py-3">
+          <h2 className="text-center text-primary"> Extrato das Vendas !</h2>
+        </div>
+
 
         <DataTable />
 
       </div>
+
       <Footer />
     </>
   );
