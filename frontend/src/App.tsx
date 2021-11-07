d39466c32e31525f6340769b15ab3dc57dca7985
Breno Neto <brenoneto14@hotmail.com>
2021-11-07 18:50:52 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,8 @@
-import DonutChart from "components/DonutChart";
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-
-      <div className="container">
-        <h1 className="text-center text-primary py-3">Dashboard de Vendas Spring React Show !!! </h1>
-
-
-        <div className="row px-3">
-          <div className="col -sm -6">
-            <h5 className="text-center text_secundary">Positivação das Vendas (%) !</h5>
-            <BarChart />
-          </div>  
-          <div className="col -sm -6">
-            <h5 className="text-center text_secundary">Participação por Vendedor (%)!</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-
-        <div className="py-3">
-          <h2 className="text-center text-primary"> Extrato das Vendas !</h2>
-        </div>
-
-
-        <DataTable />
-
-      </div>
-
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,44 @@
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <NavBar />
+
+      <div className="container">
+        <h1 className="text-center text-primary py-3">
+          Dashboard de Vendas Spring React Show !!!{" "}
+        </h1>
+
+        <div className="row px-3">
+          <div className="col -sm -6">
+            <h5 className="text-center text_secundary">
+              Positivação das Vendas (%) !
+            </h5>
+            <BarChart />
+          </div>
+          <div className="col -sm -6">
+            <h5 className="text-center text_secundary">
+              Participação por Vendedor (%)!
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-center text-primary"> Extrato das Vendas !</h2>
+        </div>
+
+        <DataTable />
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
