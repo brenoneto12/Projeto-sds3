a11b603ed520882ccc9a490cac8fde7cbbe2fd5e
Breno Neto <brenoneto14@hotmail.com>
2021-11-08 00:02:12 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,12 +25,6 @@ const DunotChart = () => {
       console.log(chartData);
     });
   }, []);
-  
-
-  //const mockData = {
-  //  series: [477138, 499928, 444867, 220426, 473088],
-  //  labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  //};
 
   const options = {
     legend: {
